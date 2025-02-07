ab3f24b19b5be0ba4f637be83d5f27439eb1c243
Nikola <nikoladjuraskovic96@gmail.com>
2025-02-07 13:56:11 +0100

    fixed partials links

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -2,10 +2,10 @@
 
     <div>
 
-        <a href="../../show.php">Books</a>
-        <a href="../../insert.php">Insert book</a>
-        <a href="../../delete.php">Delete book</a>
-        <a href="../../edit.php">Edit books</a>
+        <a href="show.php">Books</a>
+        <a href="insert.php">Insert book</a>
+        <a href="delete.php">Delete book</a>
+        <a href="edit.php">Edit books</a>
 
     </div>
 </nav>
